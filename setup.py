8f56de596aa1836b58c6fba0c16a50e1f1f00ca0
Brian-Masse <50341047+Brian-Masse@users.noreply.github.com>
2022-03-04 21:43:27 -0500

    changed the name

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 
 from distutils.core import setup
 setup(
-    name='Coffee',         # How you named your package folder (MyLib)
-    packages=['Coffee'],   # Chose the same as "name"
+    name='coffee',         # How you named your package folder (MyLib)
+    packages=['coffee'],   # Chose the same as "name"
     version='1.0',      # Start with a small number and increase it with every change you make
     # Chose a license from here: https://help.github.com/articles/licensing-a-repository
     license='MIT',
